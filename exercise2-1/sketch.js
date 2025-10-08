8f4e9ab3a8c07a6841c6f18c56d404d257b76cf6
Flo :) <103675778+FlorenceGayton@users.noreply.github.com>
2025-10-08 12:33:25 +0100

    Update sketch.js

---
+++ exercise2-1/sketch.js
@@ -0,0 +1,31 @@
+let fillColour;
+let X = 300;
+let Y = 300;
+
+
+
+function setup() {
+    createCanvas(600, 600);
+    fillColour = color(random(255), random(255), random(255));
+}
+
+function draw() {
+    background(0)
+    if(keyIsPressed === true){
+        if (keyCode === UP_ARROW){ 
+            Y = Y - 5;
+        }
+        else if (keyCode === DOWN_ARROW){ 
+            Y = Y + 5;
+        }
+        else if (keyCode === RIGHT_ARROW){ 
+            X = X + 5;
+        }
+        else if (keyCode === LEFT_ARROW){ 
+            X = X - 5;
+        }
+    }      
+    fill(fillColour);
+    rectMode(CENTER);
+    square(X, Y, width - 50);
+}
